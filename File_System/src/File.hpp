cc7c9fc12c393b98d7ea0a288c0dd8944ca08e64
Aaron Sanchez <aaroncz032@gmail.com>
2025-09-15 12:06:45 -0600

    chore: file initial doxygen documentation.

--- File_System/src/File.hpp
+++ File_System/src/File.hpp
@@ -3,17 +3,58 @@
 #include <string>
 #include "iNode.hpp"
 
+/**
+ * @class File
+ * @brief Represents a file that encapsules metadata.
+ *
+ * This class provides essential file operations to perform on the entity,
+ * including the metadata retrieval.
+ */
 class File {
  private:
+  /** @brief Metadata structure containing file attributes (iNode). */
   iNode metadata;
  public:
+  /** @brief Constructs a new File object with default metadata. */
   File();
+  /** @brief Destroys the File object and releases any related resources. */
   ~File();
-  std::string getName() const;
-  std::string getType() const;
-  std::string getState() const;
+
+  /**
+   * @brief Retrieves the file name.
+   * @return File name as a string.
+   */
+  std::string getName() const {return this->metadata.name;}
+
+  /**
+   * @brief Gets the file type (e.g., directory, regular file, link).
+   * @return File type as a string.
+   */
+  std::string getType() const {return this->metadata.type;}
+
+  /**
+  * @brief Gets the current state of the file.
+  * @return File state as a string.
+  */
+  std::string getState() const {}
+
+  /**
+  * @brief Gets the length of the file in bytes.
+  * @return File length.
+  */
   uint64_t getLength() const;
-  iNode getMetadata() const;
+
+  /**
+  * @brief Retrieves the underlying iNode metadata.
+  * @return iNode structure containing file attributes.
+  */
+  inline iNode getMetadata() const {return this->metadata;}
+  /**
+  * @brief Opens the file for operations.
+  */
   void open();
+  /**
+  * @brief Closes the file and releases associated resources.
+  */
   void close();
 };
--- File_System/src/iNode.hpp
+++ File_System/src/iNode.hpp
@@ -5,16 +5,34 @@
 #include <vector>
 #include <ctime>
 
+/**
+ * @struct iNode
+ * @brief Represents the metadata of a file.
+ *
+ * The iNode contains attributes describing a file, including identification,
+ * size, timestamps, permissions, and block pointers used for data storage.
+ */
 struct iNode {
+  /** File unique identifier (inode number). */
+  uint64_t id;
+  /** File name. */
   std::string name;
+  /** File type. */
   std::string type;
+  /** File state (e.g. open, closed)*/
   std::string state;
+  /** File length in bytes*/
   uint64_t length;
-  uint64_t inodeId;
+  /** File extension type*/
   std::string fileType;
+  /** File size in blocks.*/
   uint64_t fileSize;
+  /** File creation timestamp*/
   std::time_t creationTime;
+  /** File last modification timestamp.*/
   std::time_t modificationTime;
+  /** File related block pointers.*/
   std::vector<uint64_t> BlockPointers;
+  /** File declared permissions.*/
   std::string permissions;
 };
