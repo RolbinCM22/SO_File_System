1f835d34dd6e1f42d05c4cff40dd83d794a34327
YiYhuan <yhuancr@gmail.com>
2025-09-17 21:36:31 +0000

    pruebas del nuevo Directory

--- File_System/src/main.cpp
+++ File_System/src/main.cpp
@@ -9,6 +9,19 @@ int main() {
     fs.dir.printDirectory();
     fs.saveDirectory();
     
+    Directory dir;
+    dir.addToDirectory("test1.txt", 101);
+    dir.addToDirectory("test2.txt", 202);
+    dir.addToDirectory("test3.txt", 303);
+    dir.printDirectory();
+    std::string buscar = "test2.txt";
+    uint64_t inode = dir.findInDirectory(buscar);
+    if (inode != UINT64_MAX) {
+        std::cout << "El archivo '" << buscar << "' tiene iNode: " << inode << std::endl;
+    } else {
+        std::cout << "El archivo '" << buscar << "' no se encontró en el directorio." << std::endl;
+    }
+
     /*std::cout << "Agregando archivo 'test1.txt': " << (dir.addToDirectory("test1.txt") ? "OK" : "FALLÓ") << std::endl;
     std::cout << "Agregando archivo 'test2.txt': " << (dir.addToDirectory("test2.txt") ? "OK" : "FALLÓ") << std::endl;
     std::cout << "Agregando archivo repetido 'test1.txt': " << (dir.addToDirectory("test1.txt") ? "OK" : "FALLÓ") << std::endl;
