c31d1dae0289434117f4b868a34eac337146d87f
Rolbin Castillo Matamoros <rolbincm@live.com>
2025-10-17 13:25:39 -0600

    Add heathers of PageFaultHandler and PageTableManager

---
+++ VirtualMemoryManager/src/PageFaultHandler.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "physicalmemorymanager.h"
+#include "backingstoremanager.h"
+#include "PageTableManager.h"
+
+class PageFaultHandler {
+    private:
+    PhysicalMemoryManager* physicalMemory;
+    BackingStoreManager* backingStore;
+    
+
+public:
+    /**
+     * @brief Construct a new Page Fault Handler object 
+     * 
+     * @param pm  Physical memory manager
+     * @param bs  Backing store manager
+     */
+    PageFaultHandler(PhysicalMemoryManager* pm,
+                     BackingStoreManager* bs);
+
+    /**
+     * @brief Destroy the Page Fault Handler object
+     * 
+     */
+    ~PageFaultHandler();
+
+    /**
+     * @brief Handle a page fault
+     * 
+     * @param pageNumber The page number that caused the fault
+     */
+    void handlePageFault(uint32_t pageNumber);
+}
--- VirtualMemoryManager/src/PageTableManager.h
+++ VirtualMemoryManager/src/PageTableManager.h
@@ -6,16 +6,58 @@
 
 class PageTableManager {
     private:
+        /**
+         * @brief table of page table entries
+         * 
+         */
         std::vector<PageTableEntry> pageTable;
+        /**
+         * @brief Page fault handler
+         * 
+         */
         PageFaultHandler* faultHandler;
     public:
+        /**
+         * @brief Construct a new Page Table Manager object
+         * 
+         * @param numPages  Number of pages in the page table
+         * @param handler   Page fault handler
+         */
         PageTableManager(size_t numPages, PageFaultHandler* handler);
+        /**
+         * @brief Destroy the Page Table Manager object
+         * 
+         */
         ~PageTableManager();
-
+        /**
+         * @brief Translate a virtual address to a physical address
+         * @param virtualAddress 
+         * @return uint32_t  Physical address
+         */
         uint32_t translate(uint32_t virtualAddress);
-
+        /**
+         * @brief Set the frame number for a given virtual page number
+         * @param virtualPageNumber  Virtual page number
+         * @param frameNumber  Frame number
+         */
         void setFrame(uint32_t virtualPageNumber, uint32_t frameNumber);
+        /**
+         * @brief Check if a page is present in the page table
+         * 
+         * @param virtualPageNumber Virtual page number
+         * @return true  Page is present
+         * @return false  Page is not present
+         */
         bool isPagePresent(uint32_t virtualPageNumber) const;
+        /**
+         * @brief Mark a page as modified
+         * 
+         * @param virtualPageNumber Virtual page number
+         */
         void markModified(uint32_t virtualPageNumber);
+        /**
+         * @brief Mark a page as referenced
+         * @param virtualPageNumber Virtual page number
+         */
         void markReferenced(uint32_t virtualPageNumber);
-}
+};
