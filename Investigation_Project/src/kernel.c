9e3b1e07e2e9d9f39c11453f4e7f6a4d3625d01c
Rolbin Castillo Matamoros <rolbincm@live.com>
2025-11-12 20:22:48 -0600

    Se agrego bienvenida y dibujo en ASCII, ademas de las instrucciones de ejecucion

--- Investigation_Project/src/kernel.c
+++ Investigation_Project/src/kernel.c
@@ -2,13 +2,23 @@
 #define VGA ((volatile uint16_t*)0xB8000)
 #define cols 80
 #define rows 25
-
+/**
+ * @brief   Limpia la pantalla VGA estableciendo todos los caracteres a espacios en blanco con atributo de color predeterminado.
+ * 
+ */
 static void cls(void){
     for(int i=0; i<cols*rows; i++){
         VGA[i] = (uint16_t) ' ' | ((uint16_t) 0x07 << 8);
     }
 }
-
+/**
+ * @brief  Escribe una cadena de texto en la posición (r, c) con un atributo de color.
+ * 
+ * @param r Fila donde se escribirá el texto.
+ * @param c Columna donde se escribirá el texto.
+ * @param s Puntero a la cadena de texto.
+ * @param attr Atributo de color del texto.
+ */
 static void puts_at(int r,int c,const char* s,uint8_t attr){
     for(;*s && r<rows; ++s){
         VGA[r*cols + c] = (uint16_t)((uint8_t)*s | (attr<<8));
@@ -16,10 +26,21 @@ static void puts_at(int r,int c,const char* s,uint8_t attr){
     }
 }
 
+/**
+ * @brief Núcleo principal del sistema operativo.
+ * 
+ */
 void kernel_main(void){
     cls();
-    puts_at(10, 30, "Investigation Project", 0x1E);
-    puts_at(12, 28, "Kernel Loaded Successfully!", 0x2F);
+    puts_at(2, 30, "Bienvenido al MichiKernel", 0x1E);
+    puts_at(3, 35, "Instrucciones", 0x2F);
+    puts_at(5, 30, "Kernel de prueba con un michi", 0x0A); // ← 38 chars, no se desborda
+
+    puts_at(15, 32,  "     |\\__/,|   (`\\", 0x0F);
+    puts_at(16, 32, "   _.|o o  |_   ) )", 0x0F);
+    puts_at(17, 32, "---(((---(((--------", 0x0F);
+    puts_at(19, 33, "     Meow :3", 0x0E);
+    
     while(1);
 }
 
